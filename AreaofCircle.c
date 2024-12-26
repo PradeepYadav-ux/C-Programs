0fd96d84cb08108efc63a8733fa136f579490f5a
Pradeep Yadav <pradeep.yadav.bhilai6@gmail.com>
2024-12-26 17:53:27 +0530

    Update AreaofCircle.c

--- AreaofCircle.c
+++ AreaofCircle.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     float radius;
-    printf("Enter radius: ");
+    printf("Enter Radius: ");
     scanf("%f",&radius);
     float pi = 3.1415;
     float area = pi*radius*radius5;
